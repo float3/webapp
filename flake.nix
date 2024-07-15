8ec9112f4942227e3262d6696ac1aa8ba0d3317a
hill <hill@hilll.dev>
2024-07-15 16:29:11 +0000

    _

--- flake.nix
+++ flake.nix
@@ -1,43 +1,47 @@
 {
-  description = "trolley";
+  description = "My Application";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-  }: {
-    packages = {
-      default = nixpkgs.lib.buildPythonApplication {
-        pname = "trolley";
-        version = "1.0";
-        src = ./.;
-        propagatedBuildInputs = with nixpkgs.python312Packages; [
-          gunicorn
-          flask
-          watchdog
-          pyexiftool
-          ffmpeg-python
-          pillow
-        ];
-      };
-    };
+    flake-utils,
+  }:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        packages.myapp = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python312Full
+            pkgs.python312Packages.flask
+            pkgs.python312Packages.watchdog
+            pkgs.python312Packages.pillow
+            pkgs.python312Packages.ffmpeg-python
+            pkgs.python312Packages.pyexiftool
+          ];
+        };
 
-    devShell = {
-      default = nixpkgs.mkShell {
-        buildInputs = with nixpkgs.pkgs; [
-          python312Full
-          python312Packages.gunicorn
-          python312Packages.flask
-          python312Packages.watchdog
-          python312Packages.pyexiftool
-          python312Packages.ffmpeg-python
-          python312Packages.setuptools
-          python312Packages.pillow
-        ];
-      };
-    };
-  };
+        devShell = self.packages.${system}.myapp;
+
+        nixosModules.myapp = {
+          imports = [pkgs.nixosModules.trolley];
+
+          config = {
+            services.trolleyserver = {
+              enable = true;
+              package = self.packages.${system}.myapp;
+              serviceConfig = {
+                ExecStart = "${self.packages.${system}.myapp}/bin/python myapp.py";
+                Restart = "always";
+              };
+            };
+          };
+        };
+      }
+    );
 }
